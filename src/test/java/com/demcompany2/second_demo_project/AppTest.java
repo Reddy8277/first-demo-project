9bb8a4773b1ee931c4c68493b40c462b159e955e
ReddeppaReddy <crreddy896@gmail.com>
2022-02-26 16:20:37 +0530

    added initail commit

---
+++ src/test/java/com/demcompany2/second_demo_project/AppTest.java
@@ -0,0 +1,20 @@
+package com.demcompany2.second_demo_project;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
